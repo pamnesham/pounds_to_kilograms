c97c16df1c5fbbcc2c70219a9f984aeea1817386
pamnesham <nesha002@umn.edu>
2018-07-23 06:13:07 -0500

    added kilo converter code

---
+++ convert.js
@@ -0,0 +1,9 @@
+
+var poundWeight = prompt("Enter your weight in pounds:");
+var kiloWeight = Number(poundWeight)*.454;
+
+alert("That weight in kilograms is: "+kiloWeight);
+
+
+
+console.log("Conversion complete!");
